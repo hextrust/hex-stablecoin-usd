cfa2450744e1601998f1595b245d9309391e94b9
Evelyn M <evelyn.mok@hextrust.com>
2024-07-24 13:27:58 +0800

    fix: use pragma solidity ^0.8.20

--- contracts/ERC20WithRolesUpgradeable.sol
+++ contracts/ERC20WithRolesUpgradeable.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {BlacklistableWithRolesUpgradeable} from "contracts/BlacklistableWithRolesUpgradeable.sol";
